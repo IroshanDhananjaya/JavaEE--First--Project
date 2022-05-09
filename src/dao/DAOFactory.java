10be46bdb4f8b52bd3b2934dfdf40dc071244cfc
Iroshan Dhananjaya <86369828+IroshanDhananjaya@users.noreply.github.com>
2022-05-09 23:15:31 +0530

    Create Dao Package

---
+++ src/dao/DAOFactory.java
@@ -0,0 +1,8 @@
+package dao;
+
+/**
+ * @author : Dhananjaya
+ * @since : 0.0.1
+ **/
+public class DAOFactory {
+}
---
+++ src/dao/custom/OrderDAO.java
@@ -0,0 +1,8 @@
+package dao.custom;
+
+/**
+ * @author : Dhananjaya
+ * @since : 0.0.1
+ **/
+public interface OrderDAO {
+}
